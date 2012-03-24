75adcada12a50690d3aeb3f10677a813f673c47b
Mathieu Bastian <mathieu.bastian@gmail.com>
2012-03-24 15:00:47 -0700

    Update DesktopStreaming with new Perspective system

--- DesktopStreaming/src/org/gephi/desktop/streaming/StreamingTopComponent.java
+++ DesktopStreaming/src/org/gephi/desktop/streaming/StreamingTopComponent.java
@@ -1,83 +1,82 @@
 /*
-Copyright 2008-2010 Gephi
-Authors : Mathieu Bastian <mathieu.bastian@gephi.org>, Andre Panisson <panisson@gmail.com>
-Website : http://www.gephi.org
+ Copyright 2008-2010 Gephi
+ Authors : Mathieu Bastian <mathieu.bastian@gephi.org>, Andre Panisson <panisson@gmail.com>
+ Website : http://www.gephi.org
 
-This file is part of Gephi.
+ This file is part of Gephi.
 
-DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
 
-Copyright 2011 Gephi Consortium. All rights reserved.
+ Copyright 2011 Gephi Consortium. All rights reserved.
 
-The contents of this file are subject to the terms of either the GNU
-General Public License Version 3 only ("GPL") or the Common
-Development and Distribution License("CDDL") (collectively, the
-"License"). You may not use this file except in compliance with the
-License. You can obtain a copy of the License at
-http://gephi.org/about/legal/license-notice/
-or /cddl-1.0.txt and /gpl-3.0.txt. See the License for the
-specific language governing permissions and limitations under the
-License.  When distributing the software, include this License Header
-Notice in each file and include the License files at
-/cddl-1.0.txt and /gpl-3.0.txt. If applicable, add the following below the
-License Header, with the fields enclosed by brackets [] replaced by
-your own identifying information:
-"Portions Copyrighted [year] [name of copyright owner]"
+ The contents of this file are subject to the terms of either the GNU
+ General Public License Version 3 only ("GPL") or the Common
+ Development and Distribution License("CDDL") (collectively, the
+ "License"). You may not use this file except in compliance with the
+ License. You can obtain a copy of the License at
+ http://gephi.org/about/legal/license-notice/
+ or /cddl-1.0.txt and /gpl-3.0.txt. See the License for the
+ specific language governing permissions and limitations under the
+ License.  When distributing the software, include this License Header
+ Notice in each file and include the License files at
+ /cddl-1.0.txt and /gpl-3.0.txt. If applicable, add the following below the
+ License Header, with the fields enclosed by brackets [] replaced by
+ your own identifying information:
+ "Portions Copyrighted [year] [name of copyright owner]"
 
-If you wish your version of this file to be governed by only the CDDL
-or only the GPL Version 3, indicate your decision by adding
-"[Contributor] elects to include this software in this distribution
-under the [CDDL or GPL Version 3] license." If you do not indicate a
-single choice of license, a recipient has the option to distribute
-your version of this file under either the CDDL, the GPL Version 3 or
-to extend the choice of license to its licensees as provided above.
-However, if you add GPL Version 3 code and therefore, elected the GPL
-Version 3 license, then the option applies only if the new code is
-made subject to such option by the copyright holder.
+ If you wish your version of this file to be governed by only the CDDL
+ or only the GPL Version 3, indicate your decision by adding
+ "[Contributor] elects to include this software in this distribution
+ under the [CDDL or GPL Version 3] license." If you do not indicate a
+ single choice of license, a recipient has the option to distribute
+ your version of this file under either the CDDL, the GPL Version 3 or
+ to extend the choice of license to its licensees as provided above.
+ However, if you add GPL Version 3 code and therefore, elected the GPL
+ Version 3 license, then the option applies only if the new code is
+ made subject to such option by the copyright holder.
 
-Contributor(s):
+ Contributor(s):
 
-Portions Copyrighted 2011 Gephi Consortium.
+ Portions Copyrighted 2011 Gephi Consortium.
  */
 package org.gephi.desktop.streaming;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import java.util.logging.Logger;
 import javax.swing.Action;
 import javax.swing.SwingUtilities;
-import org.gephi.desktop.perspective.plugin.OverviewPerspective;
-import org.gephi.perspective.spi.Perspective;
-import org.gephi.perspective.spi.PerspectiveMember;
-import org.openide.nodes.Node;
-import org.openide.util.NbBundle;
-import org.openide.windows.TopComponent;
-import org.openide.windows.WindowManager;
-import org.openide.util.ImageUtilities;
 import org.netbeans.api.settings.ConvertAsProperties;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
 import org.openide.explorer.ExplorerManager;
 import org.openide.explorer.ExplorerUtils;
 import org.openide.explorer.view.BeanTreeView;
 import org.openide.nodes.AbstractNode;
 import org.openide.nodes.Children;
+import org.openide.nodes.Node;
 import org.openide.util.Lookup;
-import org.openide.util.lookup.ServiceProvider;
+import org.openide.util.NbBundle;
+import org.openide.windows.TopComponent;
 
 /**
  * Top component which displays the Streaming component.
  */
 @ConvertAsProperties(dtd = "-//org.gephi.desktop.streaming//Streaming//EN",
 autostore = false)
+@TopComponent.Description(preferredID = "StreamingTopComponent",
+iconBase = "org/gephi/desktop/streaming/resources/media-stream.png",
+persistenceType = TopComponent.PERSISTENCE_ALWAYS)
+@TopComponent.Registration(mode = "layoutmode", openAtStartup = true, roles = {"overview"})
+@ActionID(category = "Window", id = "org.gephi.desktop.streaming.StreamingTopComponent")
+@ActionReference(path = "Menu/Window", position = 1400)
+@TopComponent.OpenActionRegistration(displayName = "#CTL_StreamingTopComponent",
+preferredID = "StreamingTopComponent")
 public final class StreamingTopComponent extends TopComponent implements ExplorerManager.Provider {
 
-    private static StreamingTopComponent instance;
-    /** path to the icon used by the component and its open action */
-    static final String ICON_PATH = "org/gephi/desktop/streaming/resources/media-stream.png";
-    private static final String PREFERRED_ID = "StreamingTopComponent";
-    private StreamingUIController controller;
-    private Children clientMasterChildren;
-    private StreamingTreeView tree;
-    private StreamingConnectionNode selectedNode = null;
+    private transient StreamingUIController controller;
+    private transient Children clientMasterChildren;
+    private transient StreamingTreeView tree;
+    private transient StreamingConnectionNode selectedNode = null;
 
     public StreamingTopComponent() {
 
@@ -85,8 +84,6 @@ public final class StreamingTopComponent extends TopComponent implements Explore
 
         initComponents();
         setName(NbBundle.getMessage(StreamingTopComponent.class, "CTL_StreamingTopComponent"));
-        setToolTipText(NbBundle.getMessage(StreamingTopComponent.class, "HINT_StreamingTopComponent"));
-        setIcon(ImageUtilities.loadImage(ICON_PATH, true));
 
         controller = Lookup.getDefault().lookup(StreamingUIController.class);
         controller.setTopComponent(this);
@@ -139,10 +136,10 @@ public final class StreamingTopComponent extends TopComponent implements Explore
 
     }
 
-    /** This method is called from within the constructor to
-     * initialize the form.
-     * WARNING: Do NOT modify this code. The content of this method is
-     * always regenerated by the Form Editor.
+    /**
+     * This method is called from within the constructor to initialize the form.
+     * WARNING: Do NOT modify this code. The content of this method is always
+     * regenerated by the Form Editor.
      */
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -263,51 +260,10 @@ public final class StreamingTopComponent extends TopComponent implements Explore
     private javax.swing.JButton settingsButton;
     private javax.swing.JPanel topPanel;
     // End of variables declaration//GEN-END:variables
+    private final ExplorerManager mgr = new ExplorerManager();
 
-    /**
-     * Gets default instance. Do not use directly: reserved for *.settings files only,
-     * i.e. deserialization routines; otherwise you could get a non-deserialized instance.
-     * To obtain the singleton instance, use {@link #findInstance}.
-     */
-    public static synchronized StreamingTopComponent getDefault() {
-        if (instance == null) {
-            instance = new StreamingTopComponent();
-        }
-        return instance;
-    }
-
-    /**
-     * Obtain the StreamingTopComponent instance. Never call {@link #getDefault} directly!
-     */
-    public static synchronized StreamingTopComponent findInstance() {
-        TopComponent win = WindowManager.getDefault().findTopComponent(PREFERRED_ID);
-        if (win == null) {
-            Logger.getLogger(StreamingTopComponent.class.getName()).warning(
-                    "Cannot find " + PREFERRED_ID + " component. It will not be located properly in the window system.");
-            return getDefault();
-        }
-        if (win instanceof StreamingTopComponent) {
-            return (StreamingTopComponent) win;
-        }
-        Logger.getLogger(StreamingTopComponent.class.getName()).warning(
-                "There seem to be multiple components with the '" + PREFERRED_ID
-                + "' ID. That is a potential source of errors and unexpected behavior.");
-        return getDefault();
-    }
-
-    @Override
-    public int getPersistenceType() {
-        return TopComponent.PERSISTENCE_ALWAYS;
-    }
-
-    @Override
-    public void componentOpened() {
-        // TODO add custom code on component opening
-    }
-
-    @Override
-    public void componentClosed() {
-        // TODO add custom code on component closing
+    public ExplorerManager getExplorerManager() {
+        return mgr;
     }
 
     void writeProperties(java.util.Properties p) {
@@ -317,38 +273,8 @@ public final class StreamingTopComponent extends TopComponent implements Explore
         // TODO store your settings
     }
 
-    Object readProperties(java.util.Properties p) {
-        if (instance == null) {
-            instance = this;
-        }
-        instance.readPropertiesImpl(p);
-        return instance;
-    }
-
-    private void readPropertiesImpl(java.util.Properties p) {
+    void readProperties(java.util.Properties p) {
         String version = p.getProperty("version");
         // TODO read your settings according to their version
-    }
-
-    @Override
-    protected String preferredID() {
-        return PREFERRED_ID;
-    }
-    private final ExplorerManager mgr = new ExplorerManager();
-
-    public ExplorerManager getExplorerManager() {
-        return mgr;
-    }
-
-    @ServiceProvider(service = PerspectiveMember.class)
-    public static class StreamingTopComponentPerspectiveMember implements PerspectiveMember {
-
-        public boolean isMemberOf(Perspective perspective) {
-            return perspective instanceof OverviewPerspective;
-        }
-
-        public String getTopComponentId() {
-            return StreamingTopComponent.PREFERRED_ID;
-        }
     }
 }
