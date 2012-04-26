90b0f41a1d23d026a7ab751f88dd990f6a1aad28
André Panisson <panisson@gmail.com>
2012-04-26 19:14:17 +0200

    Wrap HttpServletRequest in order to read input stream multiple times

--- StreamingServer/src/org/gephi/streaming/server/impl/jetty/StreamingServerImpl.java
+++ StreamingServer/src/org/gephi/streaming/server/impl/jetty/StreamingServerImpl.java
@@ -41,8 +41,12 @@ Portions Copyrighted 2011 Gephi Consortium.
  */
 package org.gephi.streaming.server.impl.jetty;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.security.KeyManagementException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
@@ -61,8 +65,10 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManagerFactory;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.nio.SelectChannelConnector;
@@ -269,6 +275,7 @@ public class StreamingServerImpl implements StreamingServer {
         public void service(HttpServletRequest request, HttpServletResponse response) 
                 throws ServletException, IOException {
             
+            request = new MultiReadHttpServletRequest(request);
             if (!authenticationFilter.authenticate(request, response))
                 return;
             
@@ -298,6 +305,72 @@ public class StreamingServerImpl implements StreamingServer {
             }
         }
         
+    }
+
+    private class MultiReadHttpServletRequest extends HttpServletRequestWrapper {
+
+        private byte[] body;
+
+        public MultiReadHttpServletRequest(HttpServletRequest httpServletRequest) {
+            super(httpServletRequest);
+            // Read the request body and save it as a byte array
+            InputStream inputStream = null;
+            try {
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                inputStream = httpServletRequest.getInputStream();
+                int read;
+                while ((read = inputStream.read())!=-1) {
+                    baos.write(read);
+                }
+                body = baos.toByteArray();
+            } catch (IOException ex) {
+                Exceptions.printStackTrace(ex);
+            } finally {
+                try {
+                    inputStream.close();
+                } catch (IOException ex) {
+                    Exceptions.printStackTrace(ex);
+                }
+            }
+        }
+
+        @Override
+        public ServletInputStream getInputStream() throws IOException {
+            return new ServletInputStreamImpl(new ByteArrayInputStream(body));
+        }
+
+        @Override
+        public BufferedReader getReader() throws IOException {
+            String enc = getCharacterEncoding();
+            if(enc == null) enc = "UTF-8";
+            return new BufferedReader(new InputStreamReader(getInputStream(), enc));
+        }
+
+        private class ServletInputStreamImpl extends ServletInputStream {
+
+            private InputStream is;
+
+            public ServletInputStreamImpl(InputStream is) {
+                this.is = is;
+            }
+
+            public int read() throws IOException {
+                return is.read();
+            }
+
+            public boolean markSupported() {
+                return false;
+            }
+
+            public synchronized void mark(int i) {
+                throw new RuntimeException(new IOException("mark/reset not supported"));
+            }
+
+            public synchronized void reset() throws IOException {
+                throw new IOException("mark/reset not supported");
+            }
+        }
+
     }
 
 }
